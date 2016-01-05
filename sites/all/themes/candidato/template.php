c4b15d86aa5a480973bdba63a8053971cad24cad
luis mori <luismorith@gmail.com>
2016-01-05 03:37:48 -0500

    cambios en theme

--- sites/all/themes/candidato/template.php
+++ sites/all/themes/candidato/template.php
@@ -3,3 +3,9 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function candidato_form_alter(&$form, &$form_state, $form_id) {
+    if ($form_id == 'webform_client_form_23') {
+//        $form['#attributes']['class'][] = 'form-horizontal';
+    }
+}
