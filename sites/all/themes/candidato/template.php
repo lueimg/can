bb02f2a85b0f946e2f5e7fdded1e4aaa76b940b1
luis mori <luismorith@gmail.com>
2016-01-07 01:26:41 -0500

    todo

--- sites/all/themes/candidato/template.php
+++ sites/all/themes/candidato/template.php
@@ -8,4 +8,5 @@ function candidato_form_alter(&$form, &$form_state, $form_id) {
     if ($form_id == 'webform_client_form_23') {
 //        $form['#attributes']['class'][] = 'form-horizontal';
     }
+    drupal_add_library('system', 'ui.datepicker');
 }
