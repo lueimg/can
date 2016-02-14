79a1a45caf4ce82ac05e95b9f78f6d5954faba5f
luis mori <luismorith@gmail.com>
2016-02-14 13:41:13 -0500

    social top

--- sites/all/themes/candidato/templates/page--front.tpl.php
+++ sites/all/themes/candidato/templates/page--front.tpl.php
@@ -76,7 +76,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-<div>
+<div class="top-social">
     <div class="container">
         <div class="social_2">
             <a href="https://www.facebook.com/freddyangello.sifuentesocana?fref=ts"  target="_blank" class="fa fa-twitter"></a>
